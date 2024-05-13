09f0bc1a52ca677090d3aeaa16e6eb2accd4a60d
kojicasado <110933999+kojicasado@users.noreply.github.com>
2024-05-12 23:15:48 -0300

    salvei

--- main.js
+++ main.js
@@ -253,10 +253,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-05-30T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo1 = new Date("2024-12-31T00:00:00");
+const tempoObjetivo2 = new Date("2025-02-10T00:00:00");
+const tempoObjetivo3 = new Date("2026-02-18T00:00:00");
+const tempoObjetivo4 = new Date("2030-01-30T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
